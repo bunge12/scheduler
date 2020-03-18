64d891526280713b5a13fa7a89d3269ec6bcc083
Artur Iatsko <arturiatsko@Arturs-MBP.hitronhub.home>
2020-03-18 16:24:16 -0700

    getAppointmentsForDay selector added

---
+++ src/helpers/selectors.js
@@ -0,0 +1,14 @@
+export function getAppointmentsForDay(state, day) {
+  const result = [];
+  const fnState = { ...state }
+  const found = { ...fnState.days.find(element => element.name === day) }
+
+  if (typeof found.appointments === 'undefined') {
+    return [];
+  } else {
+    found.appointments.forEach(appt => {
+      if (fnState.appointments[appt]) { result.push(fnState.appointments[appt]) }
+    })
+  }
+  return result;
+}
