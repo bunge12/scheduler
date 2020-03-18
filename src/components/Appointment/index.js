13e9c8c77478199230bd76f668c46373af48a774
Artur Iatsko <arturiatsko@Arturs-MBP.hitronhub.home>
2020-03-18 12:44:31 -0700

    Appointment component finished with stories

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,10 +1,19 @@
-// import Appointment from "components/Appointment"
-
 import React from "react";
+import Header from "./Header"
+import Show from "./Show"
+import Empty from "./Empty"
+
 import "components/Appointment/style.scss";
 
 export default function Appointment(props) {
   return (
-    <article className="appointment"></article>
+    <article className="appointment" id={props.id}>
+      <Header time={props.time} />
+      {props.interview ?
+        <Show
+          interviewer={props.interview.interviewer}
+          student={props.interview.student}
+        /> : <Empty />}
+    </article>
   )
 }
