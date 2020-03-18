fce4d838e29959a9d533fee335e354f90fdcd7fa
Artur Iatsko <arturiatsko@Arturs-MBP.hitronhub.home>
2020-03-17 20:11:29 -0700

    Added stories for Appointment & fixed style

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,4 +1,10 @@
 // import Appointment from "components/Appointment"
 
+import React from "react";
+import "components/Appointment/style.scss";
 
-
+export default function Appointment(props) {
+  return (
+    <article className="appointment"></article>
+  )
+}
