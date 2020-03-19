7dc9a10aed8cf913ac48113e594545f6fa2d7e11
Artur Iatsko <arturiatsko@Arturs-MBP.hitronhub.home>
2020-03-19 16:56:16 -0700

    Implemented useVisualMode to display different states of the form

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,18 +2,38 @@ import React from "react";
 import Header from "./Header"
 import Show from "./Show"
 import Empty from "./Empty"
+import Form from "./Form"
 
 import "components/Appointment/style.scss";
-
+import useVisualMode from "../../hooks/useVisualMode";
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
+const interviewers = [];
 export default function Appointment(props) {
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
+
+
   return (
     <article className="appointment" id={props.id} key={props.id}>
       <Header time={props.time} />
-      {props.interview ?
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
         <Show
-          interviewer={props.interview.interviewer}
           student={props.interview.student}
-        /> : <Empty />}
+          interviewer={props.interview.interviewer}
+        />
+      )}
+      {mode === CREATE && (
+        <Form
+          interviewers={interviewers}
+          onCancel={() => back()}
+        />
+      )}
     </article>
   )
 }
