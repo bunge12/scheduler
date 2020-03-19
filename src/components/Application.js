5fdded523aaf2fcb5a3f256ae03c83b0414bc3cd
Artur Iatsko <arturiatsko@Arturs-MBP.hitronhub.home>
2020-03-18 20:13:47 -0700

    Appointments fetched from API

--- src/components/Application.js
+++ src/components/Application.js
@@ -6,45 +6,6 @@ import getAppointmentsForDay from "../helpers/selectors"
 
 import "components/Application.scss";
 
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 3,
-    time: "2pm",
-  },
-  {
-    id: 4,
-    time: "3pm",
-  },
-  {
-    id: 5,
-    time: "4pm",
-    interview: {
-      student: "Someone Student",
-      interviewer: {
-        id: 2,
-        name: "Not Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  }
-];
-
 export default function Application(props) {
 
   const [state, setState] = useState({
@@ -54,24 +15,18 @@ export default function Application(props) {
   });
 
   const setDay = day => setState({ ...state, day });
-  const setDays = days => setState(prev => ({ ...prev, days }));
+  // const setDays = days => setState(prev => ({ ...prev, days }));
 
   useEffect(() => {
-    // axios.get("/api/days").then(response => /* setDays(response.data) */ response);
     Promise.all([
       Promise.resolve(axios.get("/api/days")),
       Promise.resolve(axios.get("/api/appointments")),
     ]).then((all) => {
-      console.log(all[0]); // first
-      console.log(all[1]); // second
-
-
       setState(prev => ({ days: all[0].data, appointments: all[1].data }));
-
-
     });
   }, []);
 
+  let appointments = getAppointmentsForDay(state, state.day || "Monday")
 
   return (
     <main className="layout">
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,4 +1,4 @@
-export function getAppointmentsForDay(state, day) {
+export default function getAppointmentsForDay(state, day) {
   const result = [];
   const fnState = { ...state }
   const found = { ...fnState.days.find(element => element.name === day) }
