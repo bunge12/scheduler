27c6b8a95eea128f0f63c20cd314e79a0881a95d
Artur Iatsko <arturiatsko@Arturs-MBP.hitronhub.home>
2020-03-19 15:25:40 -0700

    Transition, Back implemented

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,3 +1,24 @@
+import React, { useState } from "react";
+
 export default function useVisualMode(initial) {
-  return { mode: initial }
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+
+  const transition = (newMode) => {
+    setHistory([...history, newMode])
+    setMode(newMode)
+  }
+
+  const back = () => {
+
+    if (history.length > 1) {
+      history.pop()
+      const lastItem = history[history.length - 1]
+      setMode(lastItem)
+      setHistory(history)
+    }
+
+  }
+  return { mode, transition, back };
 }
