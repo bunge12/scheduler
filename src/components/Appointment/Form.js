9ff6a7614422af7d381ff4306234f3ad12989934
Artur Iatsko <arturiatsko@Arturs-MBP.hitronhub.home>
2020-03-23 15:17:29 -0700

    Form tests added with student name validation

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,51 +1,72 @@
 import React, { useState } from "react";
-import Button from "../Button"
-import InterviewerList from "../InterviewList"
-
-
+import Button from "../Button";
+import InterviewerList from "../InterviewList";
 
 export default function Form(props) {
-
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
   const [name, setName] = useState(props.name || "");
+  const [error, setError] = useState("");
 
   const reset = () => {
     setName("");
     setInterviewer(null);
-  }
+  };
 
   const cancel = () => {
     props.onCancel();
     reset();
+  };
+
+  function validate() {
+    if (name === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+
+    props.onSave(name, interviewer);
   }
 
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off" onSubmit={event => event.preventDefault()} >
+        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
             type="text"
             placeholder="Enter Student Name"
+            data-testid="student-name-input"
             value={name}
             onChange={event => setName(event.target.value)}
           />
         </form>
+        <section className="appointment__validation">{error}</section>
         <InterviewerList
           interviewers={props.interviewers}
           value={interviewer}
-
-          onChange={setInterviewer} />
-
-
+          onChange={setInterviewer}
+        />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button onClick={() => { cancel() }} danger>Cancel</Button>
-          <Button onClick={() => { props.onSave(name, interviewer); }} confirm>Save</Button>
+          <Button
+            onClick={() => {
+              cancel();
+            }}
+            danger
+          >
+            Cancel
+          </Button>
+          <Button
+            onClick={() => {
+              validate();
+            }}
+            confirm
+          >
+            Save
+          </Button>
         </section>
       </section>
     </main>
-  )
+  );
 }
