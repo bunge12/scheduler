6cc6ca325ee11ad845cf08c5fda2c69ab0b5db02
Artur Iatsko <arturiatsko@Arturs-MBP.hitronhub.home>
2020-03-24 16:11:04 -0700

    Edit tests added

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -10,7 +10,8 @@ import {
   getAllByTestId,
   getByAltText,
   getByPlaceholderText,
-  queryByText
+  queryByText,
+  queryByAltText
 } from "@testing-library/react";
 
 import Application from "components/Application";
@@ -75,5 +76,41 @@ describe("Application Component", () => {
       queryByText(day, "Monday")
     );
     expect(getByText(day, /2 spots remaining/i)).toBeInTheDocument();
+  });
+
+  it("loads data, edits an interview and keeps the spots remaining for Monday", async () => {
+    // 1. Render the Application.
+    const { container } = render(<Application />);
+
+    // 2. Wait until the text "Archie Cohen" is displayed.
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+
+    // 3. Click the "Edit" button on the booked appointment.
+    const appointment = getAllByTestId(
+      container,
+      "appointment"
+    ).find(appointment => queryByText(appointment, "Archie Cohen"));
+
+    fireEvent.click(queryByAltText(appointment, "Edit"));
+
+    // 4. Check that the form is diaplyed with Student Name.
+    const input = getByPlaceholderText(appointment, "Enter Student Name");
+    expect(input.value).toBe("Archie Cohen");
+
+    // 5. Click the "Save" button on the confirmation.
+    fireEvent.click(queryByText(appointment, "Save"));
+
+    // 6. Check that the element with the text "Deleting" is displayed.
+    expect(getByText(appointment, /saving/i)).toBeInTheDocument();
+
+    // 7. Wait until the element with the text "Archie Cohen" is displayed
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+
+    // 8. Check that the DayListItem with the text "Monday" also has the text "1 spot remaining".
+    const day = getAllByTestId(container, "day").find(day =>
+      queryByText(day, "Monday")
+    );
+
+    expect(getByText(day, /no spots remaining/i)).toBeInTheDocument();
   });
 });
