2f6b6480b3a7e3bebd9431fc0b5883de83556179
Artur <54365622+bunge12@users.noreply.github.com>
2020-05-11 12:14:25 -0700

    Updated proxy to Heroku and counter

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -6,20 +6,20 @@ export default function useApplicationData() {
     day: "Monday",
     days: [],
     interviewers: {},
-    appointments: {}
+    appointments: {},
   });
 
   useEffect(() => {
     Promise.all([
       Promise.resolve(axios.get("/api/days")),
       Promise.resolve(axios.get("/api/appointments")),
-      Promise.resolve(axios.get("/api/interviewers"))
-    ]).then(all => {
-      setState(prev => ({
+      Promise.resolve(axios.get("/api/interviewers")),
+    ]).then((all) => {
+      setState((prev) => ({
         day: "Monday",
         days: all[0].data,
         interviewers: all[2].data,
-        appointments: all[1].data
+        appointments: all[1].data,
       }));
     });
   }, []);
@@ -27,63 +27,66 @@ export default function useApplicationData() {
   const bookInterview = (id, interview) => {
     const appointment = {
       ...state.appointments[id],
-      interview: { ...interview }
+      interview: { ...interview },
     };
     const appointments = {
       ...state.appointments,
-      [id]: appointment
+      [id]: appointment,
     };
 
-    return axios.put(`/api/appointments/${id}`, appointment).then(res => {
-      state.days.forEach(item => {
+    return axios.put(`/api/appointments/${id}`, appointment).then((res) => {
+      state.days.forEach((item) => {
         const arr = item.appointments;
         const appt = state.appointments[id].id;
-        const found = arr.find(element => element === appt);
+        const found = arr.find((element) => element === appt);
         if (typeof found !== "undefined") {
+          // console.log();
           const x = item.id - 1;
-          const newSpots = state.days[x].spots - 1;
+          const newSpots = !state.appointments[id].interview
+            ? state.days[x].spots - 1
+            : state.days[x].spots;
           const day = {
             ...state.days[x],
-            spots: newSpots
+            spots: newSpots,
           };
           const days = [...state.days];
           days[x] = day;
-          setState(prev => ({ ...state, appointments, days }));
+          setState((prev) => ({ ...state, appointments, days }));
         }
       });
     });
   };
-  const cancelInterview = id => {
+  const cancelInterview = (id) => {
     const appointment = {
       ...state.appointments[id],
-      interview: null
+      interview: null,
     };
     const appointments = {
       ...state.appointments,
-      [id]: appointment
+      [id]: appointment,
     };
 
-    return axios.delete(`/api/appointments/${id}`).then(res => {
-      state.days.forEach(item => {
+    return axios.delete(`/api/appointments/${id}`).then((res) => {
+      state.days.forEach((item) => {
         const arr = item.appointments;
         const appt = state.appointments[id].id;
-        const found = arr.find(element => element === appt);
+        const found = arr.find((element) => element === appt);
         if (typeof found !== "undefined") {
           const x = item.id - 1;
           const newSpots = state.days[x].spots + 1;
           const day = {
             ...state.days[x],
-            spots: newSpots
+            spots: newSpots,
           };
           const days = [...state.days];
           days[x] = day;
-          setState(prev => ({ ...state, appointments, days }));
+          setState((prev) => ({ ...state, appointments, days }));
         }
       });
     });
   };
 
-  const setDay = day => setState({ ...state, day });
+  const setDay = (day) => setState({ ...state, day });
 
   return { state, setDay, bookInterview, cancelInterview };
 }
